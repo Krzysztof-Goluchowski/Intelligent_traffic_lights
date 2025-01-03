cbe0240065790c4542ff99c428ae99fb9ac6da14
Krzysztof Gołuchowski <kgoluchowski@student.agh.edu.pl>
2025-01-03 19:32:03 +0100

    Refactor: Move logic from Simulation to IntersectionManager

---
+++ Intelligent_traffic_lights/src/main/java/recruitmentTask/intersection/IntersectionManager.java
@@ -0,0 +1,172 @@
+package recruitmentTask.intersection;
+
+import recruitmentTask.command.Command;
+import recruitmentTask.road.Direction;
+import recruitmentTask.road.Road;
+import recruitmentTask.vehicle.Vehicle;
+import recruitmentTask.vehicle.VehicleFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IntersectionManager {
+    private final IntersectionController intersectionController;
+    private final List<List<String>> stepStatuses;
+    private final Map<Direction, Road> roads;
+    private final List<Vehicle> vehiclesAtIntersection = new ArrayList<>();
+
+    public IntersectionManager(IntersectionController intersectionController, List<List<String>> stepStatuses) {
+        this.intersectionController = intersectionController;
+        this.stepStatuses = stepStatuses;
+        this.roads = initializeRoads();
+    }
+
+    private Map<Direction, Road> initializeRoads() {
+        Map<Direction, Road> roads = new HashMap<>();
+        for (Direction direction : Direction.values()) {
+            roads.put(direction, new Road(direction));
+        }
+        return roads;
+    }
+
+    public void addVehicle(Command command) {
+        Vehicle vehicle = VehicleFactory.create(command);
+        roads.get(command.getStartRoad()).addVehicle(vehicle);
+    }
+
+    public void step() {
+        List<Direction> busyRoads = getBusyRoads();
+        if (isNeedForChange(busyRoads)) {
+            intersectionController.handleTraffic(busyRoads);
+            leaveIntersection();
+        }
+        List<String> leftVehicles = moveVehicles(busyRoads);
+
+        recordStepStatus(leftVehicles);
+    }
+
+    private List<Direction> getBusyRoads() {
+        int northSouthTrafficLoad = getVehicleCount(Direction.north) + getVehicleCount(Direction.south);
+        int westEastTrafficLoad = getVehicleCount(Direction.west) + getVehicleCount(Direction.east);
+
+        if (northSouthTrafficLoad >= westEastTrafficLoad) {
+            return Direction.northSouth();
+        }
+
+        return Direction.westEast();
+    }
+
+    private int getVehicleCount(Direction direction) {
+        return roads.get(direction).getVehicleCount();
+    }
+
+    private boolean isNeedForChange(List<Direction> directions) {
+        return !intersectionController.isGreenOn(directions.getFirst());
+    }
+
+    private void leaveIntersection() {
+        for (Vehicle vehicle : vehiclesAtIntersection) {
+            stepStatuses.getLast().add(vehicle.vehicleId());
+        }
+
+        vehiclesAtIntersection.clear();
+    }
+
+    private List<String> moveVehicles(List<Direction> directions) {
+        List<String> leftVehicles = new ArrayList<>();
+
+        enterIntersection(directions);
+        leftVehicles.addAll(exitIntersection());
+
+        return leftVehicles;
+    }
+
+    private void enterIntersection(List<Direction> directions) {
+        Direction dir1 = directions.get(0);
+        Direction dir2 = directions.get(1);
+
+        if (vehiclesAtIntersection.isEmpty()) {
+            Vehicle vehicle1 = roads.get(dir1).removeVehicle();
+            Vehicle vehicle2 = roads.get(dir2).removeVehicle();
+
+            if (vehicle1 != null) {
+                vehiclesAtIntersection.add(vehicle1);
+            }
+            if (vehicle2 != null) {
+                vehiclesAtIntersection.add(vehicle2);
+            }
+
+            return;
+        }
+
+        Direction opositeDirection = getOpositeDirection(vehiclesAtIntersection.getFirst().startRoad());
+        Vehicle vehicle = roads.get(opositeDirection).removeVehicle();
+
+        if (vehicle != null) {
+            vehiclesAtIntersection.add(vehicle);
+        }
+    }
+
+    private Direction getOpositeDirection(Direction direction) {
+        return Direction.getOpositeDirection(direction);
+    }
+
+    private List<String> exitIntersection() {
+        List<String> leftVehicles = new ArrayList<>();
+
+        if (vehiclesAtIntersection.size() == 1) {
+            leftVehicles.add(vehiclesAtIntersection.getFirst().vehicleId());
+            vehiclesAtIntersection.clear();
+        }
+
+        if (vehiclesAtIntersection.size() == 2) {
+            leftVehicles.addAll(resolveIntersection());
+        }
+
+        return leftVehicles;
+    }
+
+    private List<String> resolveIntersection() {
+        Vehicle vehicle1 = vehiclesAtIntersection.get(0);
+        Vehicle vehicle2 = vehiclesAtIntersection.get(1);
+
+        boolean priority1 = havePriority(vehicle1);
+        boolean priority2 = havePriority(vehicle2);
+
+        if (priority1 && priority2 || !priority1 && !priority2) {
+            vehiclesAtIntersection.remove(vehicle1);
+            vehiclesAtIntersection.remove(vehicle2);
+            return List.of(vehicle1.vehicleId(), vehicle2.vehicleId());
+        }
+
+        if (priority1) {
+            vehiclesAtIntersection.remove(vehicle1);
+            return List.of(vehicle1.vehicleId());
+        } else {
+            vehiclesAtIntersection.remove(vehicle2);
+            return List.of(vehicle2.vehicleId());
+        }
+    }
+
+    private boolean havePriority(Vehicle vehicle) {
+        return !Direction.isTurningLeft(vehicle.startRoad(), vehicle.endRoad());
+    }
+
+    private void recordStepStatus(List<String> leftVehicles) {
+        stepStatuses.add(leftVehicles);
+    }
+
+    public List<List<String>> getStepStatuses() {
+        return stepStatuses;
+    }
+
+    public Map<Direction, Road> getRoads() {
+        return roads;
+    }
+
+    public List<Vehicle> getVehiclesAtIntersection() {
+        return vehiclesAtIntersection;
+    }
+}
--- Intelligent_traffic_lights/src/main/java/recruitmentTask/simulation/Simulation.java
+++ Intelligent_traffic_lights/src/main/java/recruitmentTask/simulation/Simulation.java
@@ -1,178 +1,31 @@
 package recruitmentTask.simulation;
 
-import recruitmentTask.controller.IntersectionController;
+import recruitmentTask.intersection.IntersectionController;
 import recruitmentTask.command.Command;
-import recruitmentTask.road.Direction;
-import recruitmentTask.road.Road;
-import recruitmentTask.vehicle.Vehicle;
-import recruitmentTask.vehicle.VehicleFactory;
+import recruitmentTask.intersection.IntersectionManager;
 
 import java.util.*;
 
 public class Simulation {
     private final IntersectionController intersectionController;
-    private Map<Direction, Road> roads;
+    private final IntersectionManager intersectionManager;
     private final List<List<String>> stepStatuses;
     private final List<Command> commands;
-    private final List<Vehicle> vehiclesAtTheIntersection = new ArrayList<>();
 
     public Simulation(List<Command> commands) {
         this.commands = commands;
         intersectionController = new IntersectionController();
         stepStatuses = new ArrayList<>();
-        initializeRoads();
-    }
-
-    private void initializeRoads() {
-        roads = new HashMap<>();
-        roads.put(Direction.north, new Road(Direction.north));
-        roads.put(Direction.south, new Road(Direction.south));
-        roads.put(Direction.west, new Road(Direction.west));
-        roads.put(Direction.east, new Road(Direction.east));
+        this.intersectionManager = new IntersectionManager(intersectionController, stepStatuses);
     }
 
     public void run() {
         for (Command command : commands) {
             switch (command.getCommandType()) {
-                case step -> step();
-                case addVehicle -> addVehicle(command);
+                case step -> intersectionManager.step();
+                case addVehicle -> intersectionManager.addVehicle(command);
             }
         }
-    }
-
-    private void step() {
-        List<Direction> busyRoads = getBusyRoads();
-        boolean changingLight = isNeedForChangingLight(busyRoads.getFirst());
-
-        if (changingLight) {
-            intersectionController.handleTraffic(busyRoads);
-            leaveIntersection();
-        }
-
-        List<String> leftVehicles = moveVehicles(busyRoads);
-
-        recordStepStatus(leftVehicles);
-    }
-
-    private List<Direction> getBusyRoads() {
-        int northSouthTrafficLoad = getVehicleCount(Direction.north) + getVehicleCount(Direction.south);
-        int westEastTrafficLoad = getVehicleCount(Direction.west) + getVehicleCount(Direction.east);
-
-        if (northSouthTrafficLoad > westEastTrafficLoad) {
-            return Direction.northSouth();
-        }
-
-        return Direction.westEast();
-    }
-
-    private int getVehicleCount(Direction direction) {
-        return roads.get(direction).getVehicleCount();
-    }
-
-    private boolean isNeedForChangingLight(Direction direction) {
-        return !intersectionController.isGreenOn(direction);
-    }
-
-    private void leaveIntersection() {
-        for (Vehicle vehicle : vehiclesAtTheIntersection) {
-            stepStatuses.getLast().add(vehicle.vehicleId());
-        }
-
-        vehiclesAtTheIntersection.clear();
-    }
-
-    private List<String> moveVehicles(List<Direction> directions) {
-        List<String> leftVehicles = new ArrayList<>();
-
-        enterIntersection(directions);
-        leftVehicles.addAll(exitIntersection());
-
-        return leftVehicles;
-    }
-
-    private List<String> exitIntersection() {
-        List<String> leftVehicles = new ArrayList<>();
-
-        if (vehiclesAtTheIntersection.size() == 1) {
-            leftVehicles.add(vehiclesAtTheIntersection.getFirst().vehicleId());
-            vehiclesAtTheIntersection.clear();
-        }
-
-        if (vehiclesAtTheIntersection.size() == 2) {
-            leftVehicles.addAll(resolveIntersection());
-        }
-
-        return leftVehicles;
-    }
-
-    private List<String> resolveIntersection() {
-        Vehicle vehicle1 = vehiclesAtTheIntersection.get(0);
-        Vehicle vehicle2 = vehiclesAtTheIntersection.get(1);
-
-        boolean priority1 = havePriority(vehicle1);
-        boolean priority2 = havePriority(vehicle2);
-
-        if (priority1 && priority2 || !priority1 && !priority2) {
-            vehiclesAtTheIntersection.remove(vehicle1);
-            vehiclesAtTheIntersection.remove(vehicle2);
-            return List.of(vehicle1.vehicleId(), vehicle2.vehicleId());
-        }
-
-        if (priority1) {
-            vehiclesAtTheIntersection.remove(vehicle1);
-            return List.of(vehicle1.vehicleId());
-        } else {
-            vehiclesAtTheIntersection.remove(vehicle2);
-            return List.of(vehicle2.vehicleId());
-        }
-    }
-
-    private void enterIntersection(List<Direction> directions) {
-        Direction dir1 = directions.get(0);
-        Direction dir2 = directions.get(1);
-
-        if (vehiclesAtTheIntersection.isEmpty()) {
-            Vehicle vehicle1 = roads.get(dir1).removeVehicle();
-            Vehicle vehicle2 = roads.get(dir2).removeVehicle();
-
-            if (vehicle1 != null) {
-                vehiclesAtTheIntersection.add(vehicle1);
-            }
-            if (vehicle2 != null) {
-                vehiclesAtTheIntersection.add(vehicle2);
-            }
-
-            return;
-        }
-
-        Direction opositeDirection = getOpositeDirection(vehiclesAtTheIntersection.getFirst().startRoad());
-        Vehicle vehicle = roads.get(opositeDirection).removeVehicle();
-
-        if (vehicle != null) {
-            vehiclesAtTheIntersection.add(vehicle);
-        }
-    }
-
-    private Direction getOpositeDirection(Direction direction) {
-        return Direction.getOpositeDirection(direction);
-    }
-
-    private boolean havePriority(Vehicle vehicle) {
-        return !Direction.isTurningLeft(vehicle.startRoad(), vehicle.endRoad());
-    }
-
-    private void recordStepStatus(List<String> leftVehicles) {
-        stepStatuses.add(leftVehicles);
-    }
-
-    private void addVehicle(Command command) {
-        Road road = roads.get(command.getStartRoad());
-        Vehicle vehicle = VehicleFactory.create(command);
-        road.addVehicle(vehicle);
-    }
-
-    public Map<Direction, Road> getRoads() {
-        return roads;
     }
 
     public List<List<String>> getStepStatuses() {
@@ -181,10 +34,6 @@ public class Simulation {
 
     public List<Command> getCommands() {
         return commands;
-    }
-
-    public List<Vehicle> getVehiclesAtTheIntersection() {
-        return vehiclesAtTheIntersection;
     }
 
     public IntersectionController getIntersectionController() {
